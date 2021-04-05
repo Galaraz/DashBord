320cdd74aea99850e7859d0cd016e17b8018ee1c
Galaraz <jvictor.si.2013@gmail.com>
2021-04-04 21:49:06 -0300

    aplicando logica no olho da tormenta agora vai

--- src/components/Forms/OrderForm.js
+++ src/components/Forms/OrderForm.js
@@ -3,6 +3,7 @@ import Grid from '@material-ui/core/Grid';
 import Controls from '../Controls/Controls';
 import { CustomerForm, Form } from '../Forms/CustomForm';
 import * as employeeService from '../../services/employeeService';
+import Title from '../BaseDasbord/Title';
 
 const typeItems = [
   { id: 'despesa', title: 'Despesa' },
@@ -29,12 +30,12 @@ export default function Orderpage() {
     if ('mobile' in fieldValues)
       temp.mobile = fieldValues.mobile.length > 9 ? '' : 'Minimum 10 numbers required.';
     if ('departmentId' in fieldValues)
-      temp.departmentId = fieldValues.departmentId.length != 0 ? '' : 'This field is required.';
+      temp.departmentId = fieldValues.departmentId.length !== 0 ? '' : 'This field is required.';
     setErrors({
       ...temp,
     });
 
-    if (fieldValues == values) return Object.values(temp).every((x) => x == '');
+    if (fieldValues == values) return Object.values(temp).every((x) => x === '');
   };
 
   const { values, setValues, errors, setErrors, handleInputChange, resetForm } = CustomerForm(
@@ -56,6 +57,7 @@ export default function Orderpage() {
       <Form onSubmit={handleSubmit}>
         <Grid container>
           <Grid item xs={12} sm={12} md={6} lg={6}>
+          <Title>Depósitos Recentes</Title>
             <Controls.Select
               name="receptor"
               label="Receptor"
